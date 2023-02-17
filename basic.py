b0a421e40af52d2b1623f91f8718fb48e139a8e9
krasenHristov <DoAnuSho@gmail.com>
2023-02-17 09:43:19 +0000

    updating basic

--- basic.py
+++ basic.py
@@ -1,13 +1,4 @@
-def calculation(height, width, coverage
-) :
-    result = (height * width) / coverage
-    if result < 2 :
-        print('2')
-    else :
-        print(f'{round(result)}')
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letter = input('input')
 
-calculation(
-    height = int(input('Height of the wall\n')),
-width = int(input('width of the wall\n')),
-coverage = 5 
-)
+print(alphabet.index(letter) + (13 - 1))
