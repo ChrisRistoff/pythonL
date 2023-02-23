8d80edec687d6b37a5c8bc87977ce6bf2830626a
krasenHristov <DoAnuSho@gmail.com>
2023-02-23 14:59:47 +0000

    calculator

---
+++ basic.py
@@ -0,0 +1,7 @@
+def sum(a,b):
+    return a+b
+
+def calc(a, b) :
+    return sum(a, b)
+
+g = calc 
---
+++ calculator.py
@@ -0,0 +1,43 @@
+def add(n1, n2) :
+    return n1 + n2
+
+def multiply(n1, n2) :
+   return n1 * n2
+
+def divide(n1 ,n2) :
+    return n1 / n2
+
+def minus(n1 ,n2) :
+    return n1 - n2
+
+def calculation (n1, operand) :
+    while operand != "*" and operand != "+" and operand != "-" and operand != "/" :
+        operand = str(input("Choose an operation * + - /\n"))
+    number_two = int(input("Choose a number\n"))
+    if operand == "*" :
+        g = multiply(n1, number_two)
+    elif operand == "-" :
+        g = minus(n1, number_two)
+    elif operand == "+" :
+        g = add(n1, number_two)
+    else :
+        g = divide(n1, number_two)
+    return g
+
+def go_again(a) :
+    while a != "yes" :
+        if a == "no" :
+            break
+        else :
+            a = input("would you like to go again?\n")
+    return a
+
+number_one = int(input("Choose a number\n"))
+again = ""
+operation =""
+new_number = 0
+
+while again != "no" :
+    number_one = calculation(number_one, operation)
+    print(number_one)
+    go_again(again)
