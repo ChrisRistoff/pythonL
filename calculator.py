9783dce21b4c08bd88922414108408e53fed2787
krasenHristov <DoAnuSho@gmail.com>
2023-02-24 11:35:01 +0000

    calc

--- calculator.py
+++ calculator.py
@@ -20,17 +20,13 @@ def calculation (n1, operand,dic) :
     number_two = int(input("Choose a number\n"))
     return dic[operand](n1, number_two)
 
-def go_again(a) :
-    while a != "yes" :
-        if a == "no" :
-            break
-        else :
-            a = input("would you like to go again?\n")
-    return a
+
 
 number_one = int(input("Choose a number\n"))
 again = ""
 while again != "no" :
     number_one = calculation(number_one, "", operations)
     print(number_one)
-    again = go_again("")
+    again = ""
+    while again != "yes" and again != "no" :
+        again = input("Would you like to go again")
