9bd919d4c4edaff05e09b5a332e7d88173768903
krasenHristov <DoAnuSho@gmail.com>
2023-02-25 23:56:14 +0000

    .

--- blackjack.py
+++ blackjack.py
@@ -3,7 +3,7 @@ import time
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def hand_deal(): 
+def deal_hand(): 
     the_hand = []
     while len(the_hand) < 2 :
         the_hand.append(cards[random.randint(0,12)])
@@ -11,8 +11,8 @@ def hand_deal():
         the_hand[1] = 1
     return the_hand
 
-def player_hand_play() :
-    p_hand = hand_deal()
+def play_player_hand() :
+    p_hand = deal_hand()
     print(f"{p_hand} score : {sum(p_hand)}")
     if sum(p_hand) == 21:
         time.sleep(1)
@@ -42,10 +42,11 @@ def player_hand_play() :
         print(f"score : {sum(p_hand)} BUST! YOU LOSE")
         return p_hand
 
-def computer_hand_play(pl_hand, c_hand):
+def play_computer_hand(pl_hand, c_hand):
     if sum(pl_hand) > 21 :
         return
     while sum(c_hand) < sum(pl_hand):
+        time.sleep(1)
         c_hand.append(cards[random.randint(0,12)])
         print(f"{c_hand} score : {sum(c_hand)}")
         if c_hand[-1] == 11:
@@ -57,8 +58,7 @@ def computer_hand_play(pl_hand, c_hand):
         time.sleep(1)
         print(f"Computer is BUST with {sum(c_hand)} score, YOU WON!")
         return c_hand
-    else :
-        return c_hand
+    return c_hand
 
 def play_again_prompt() :
     a = ""
@@ -68,23 +68,24 @@ def play_again_prompt() :
         return a
     else :
         return a
+    
 play = ""
 while play != "no":
-    comp_hand = hand_deal()
+    comp_hand = deal_hand()
     print(f"[{comp_hand[0]}][[]]")
     if comp_hand == 21 :
         print(f"{comp_hand} BLACKJACK! Computer wins")
         play = play_again_prompt()
         continue
     time.sleep(1)
-    your_hand = player_hand_play()
-    if your_hand is True :
+    your_hand = play_player_hand()
+    if your_hand is True:
         play = play_again_prompt()
         continue
     print(f"Computer hand\n {comp_hand}")
-    comp_hand = computer_hand_play(your_hand, comp_hand)
-    play_again = ""
-
+    time.sleep(1)
+    comp_hand = play_computer_hand(your_hand, comp_hand)
+    
     if sum(your_hand) > 21 or sum(comp_hand) > 21 :
         ""
     else:
